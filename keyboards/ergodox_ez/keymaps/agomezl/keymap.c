9e0fb0e44833b7e147322db35c098365145c2fa9
Alejandro Gomez-Londono <alegomez544@gmail.com>
2018-09-25 15:59:39 +0200

    Adding nice HOL macros

--- keyboards/ergodox_ez/keymaps/agomezl/keymap.c
+++ keyboards/ergodox_ez/keymaps/agomezl/keymap.c
@@ -25,32 +25,41 @@ enum custom_keycodes {
   HOL_F,
   HOL_H,
   HOL_R,
+  HOL_FS,
+  HOL_RFS,
+  HOL_MET,
+  HOL_RW,
+  HOL_CSE,
+  HOL_IND,
+  HOL_FND,
+  HOL_MCH,
   HOL_TRM,
   HOL_QTE,
   HOL_THN,
-  HOL_APP
+  HOL_APP,
+  ML_CMNT
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_ESC,   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,
-        KC_GRAVE, KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    XXXXXXX,
-        KC_TAB,   KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    XXXXXXX,
-        KC_LCTRL, XXXXXXX, XXXXXXX, XXXXXXX, KC_LALT,
-                                                        XXXXXXX,  XXXXXXX,
-                                                                  XXXXXXX,
-                                             KC_LGUI,   MO(MOVE), HOL_H,
+        KC_ESC,   HOL_RW,  HOL_FS,  HOL_MET, HOL_CSE,  HOL_IND,  KC_LBRC,
+        KC_GRAVE, KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,     KC_SLSH,
+        KC_TAB,   KC_A,    KC_S,    KC_D,     KC_F,    KC_G,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,     XXXXXXX,
+        KC_LCTRL, XXXXXXX, XXXXXXX, XXXXXXX,  KC_LGUI,
+                                                       XXXXXXX,  XXXXXXX,
+                                                                 XXXXXXX,
+                                              KC_LALT, MO(MOVE), HOL_H,
         // right hand
-        XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, KC_BSPC,
-        XXXXXXX,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_BSLS,
-                  KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN, KC_ENT,
-        XXXXXXX,  KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, MO(SYMB),
-                           KC_LPRN, KC_RPRN,  KC_LBRC,  KC_RBRC, KC_RCTRL,
+        KC_RBRC,  HOL_FND,  HOL_MCH, XXXXXXX,  XXXXXXX,  XXXXXXX, KC_BSPC,
+        KC_BSLS,  KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,    KC_BSLS,
+                  KC_H,     KC_J,    KC_K,     KC_L,     KC_SCLN, KC_ENT,
+        XXXXXXX,  KC_N,     KC_M,    KC_COMM,  KC_DOT,   KC_SLSH, MO(SYMB),
+                            KC_LPRN, KC_RPRN,  KC_LBRC,  KC_RBRC, KC_RCTRL,
         E_GO_L,   E_GO_R,
         XXXXXXX,
-        XXXXXXX,  XXXXXXX,  KC_SPC
+        XXXXXXX,  KC_RCTRL, KC_SPC
     ),
 
 // MOVEMENT
@@ -59,14 +68,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LBRC_RBRC, _______, _______, _______, _______, _______, _______,
         _______,   KC_VOLD, KC_VOLU, KC_END,  _______, _______, _______,
         KC_QUOTE,  KC_HOME, _______, KC_DEL,  HOL_F  , _______,
-        _______,   _______, HOL_X,   E_LAST,  _______, _______, _______,
+        _______,   ML_CMNT, HOL_X,   E_LAST,  _______, _______, _______,
         _______,   _______, _______, _______, _______,
                                                        _______, _______,
                                                                 _______,
                                               _______, _______, _______,
         // right hand
         _______,  _______, _______, _______,  _______,   _______, HOL_APP,
-        _______,  HOL_Y,   KC_PGUP, KC_UP,    KC_PGDOWN, HOL_P,   _______,
+        _______,  HOL_Y,   KC_PGUP, KC_UP,    KC_PGDOWN, HOL_P,   HOL_THN,
                   _______, KC_LEFT, KC_DOWN,  KC_RIGHT,  KC_INS,  _______,
         _______,  _______, _______, _______,  _______,   _______, LPAR_RPAR,
                            DUAL_QU, HOL_QTE,  _______,   _______, HOL_TRM,
@@ -173,6 +182,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       SEND_STRING(SS_LCTRL("c "));
       return false;
       break;
+    case ML_CMNT:
+      SEND_STRING("(*  *)"SS_TAP(X_LEFT)SS_TAP(X_LEFT)SS_TAP(X_LEFT));
+      return false;
+      break;
     case HOL_X:
       SEND_STRING(SS_LCTRL("x")"1"SS_LALT("h")"3");
       return false;
@@ -219,11 +232,53 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
       break;
     case HOL_THN:
-      SEND_STRING("\\\\");
+      SEND_STRING("\\\\\\\\ ");
       return false;
       break;
     case HOL_APP:
       SEND_STRING(" |> ");
+      return false;
+      break;
+    case HOL_FS:
+      SEND_STRING("fs []"SS_TAP(X_LEFT));
+      return false;
+      break;
+    case HOL_RFS:
+      SEND_STRING("rfs []"SS_TAP(X_LEFT));
+      return false;
+      break;
+    case HOL_RW:
+      SEND_STRING("rw []"SS_TAP(X_LEFT));
+      return false;
+      break;
+    case HOL_CSE:
+      SEND_STRING("Cases_on ` ` "SS_TAP(X_LEFT));
+      return false;
+      break;
+    case HOL_IND:
+      SEND_STRING("Induct_on ` ` "SS_TAP(X_LEFT));
+      return false;
+      break;
+    case HOL_MET:
+      SEND_STRING("METIS_TAC []"SS_TAP(X_LEFT));
+      return false;
+      break;
+    case HOL_FND:
+      SEND_STRING(// open hol window
+                  SS_LCTRL("x")"1"SS_LALT("h")"3"
+                  // move to hol window
+                  SS_LCTRL("x")SS_TAP(X_RIGHT)
+                  // write in it
+                  "print_find \" \" ;"SS_TAP(X_LEFT)SS_TAP(X_LEFT));
+      return false;
+      break;
+    case HOL_MCH:
+      SEND_STRING(// open hol window
+                  SS_LCTRL("x")"1"SS_LALT("h")"3"
+                  // move to hol window
+                  SS_LCTRL("x")SS_TAP(X_RIGHT)
+                  // write in it
+                  "print_match [] \" \" ;"SS_TAP(X_LEFT)SS_TAP(X_LEFT));
       return false;
       break;
     }
