73c2bdda1157797c788c3523bccba32e315db265
Alejandro Gomez-Londono <alegomez544@gmail.com>
2019-02-21 20:52:48 +0100

    Add better version

--- keyboards/ergodox_ez/keymaps/agomezl/keymap.c
+++ keyboards/ergodox_ez/keymaps/agomezl/keymap.c
@@ -45,18 +45,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [LY0] = LAYOUT_ergodox(  // layer 0 : default
         //  left hand
         KC_ESC,  HOL_RW,  HOL_FS,  HOL_MET, HOL_CSE, HOL_IND, KC_LBRC,
-        KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MEH_T(KC_NO),
+        KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    E_GO_L,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    E_GO_L,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TG(LY2),
         KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI,
                                                      XXXXXXX, XXXXXXX,
                                                               XXXXXXX,
                                             KC_LALT, MO(LY3), HOL_H,
         // right hand
         KC_RBRC, HOL_FND, HOL_MCH, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
-   MEH_T(KC_NO), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        E_GO_R,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        E_GO_R, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(LY4),
+   MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(LY4),
                           KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_RCTRL,
         XXXXXXX, XXXXXXX,
         XXXXXXX,
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Symbols
 [LY4] = LAYOUT_ergodox(
         // left hand
-        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,
+        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,
         _______,  _______, _______, _______, _______, _______,  _______,
         KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
         _______,  _______, _______, _______, _______, _______,  _______,
