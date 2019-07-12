71834976f4cee65dd5aacf00b2ad3278b0c7d063
Alejandro Gomez-Londono <alegomez544@gmail.com>
2019-07-12 15:35:49 +0200

    [agomezl/planck] Add HOL_F, bracket support

--- keyboards/planck/keymaps/agomezl/keymap.c
+++ keyboards/planck/keymaps/agomezl/keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [LY1] = LAYOUT_planck_grid(
         L_R_BRC, KC_VOLD, KC_VOLU, KC_END,  _______, _______, HOL_Y,   KC_PGUP, KC_UP,   KC_PGDN, HOL_P,   HOL_THN,
-        KC_QUOT, KC_HOME, KC_SPC,  KC_DEL,  HOL_F  , _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
+        KC_QUOT, KC_HOME, KC_SPC,  KC_DEL,  HOL_F  , _______, HOL_H,   KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
         _______, ML_CMNT, HOL_X,   E_LAST,  MO(LY3), _______, _______, _______, _______, _______, _______, L_R_PAR,
         _______, E_MAGIT, _______, _______, _______, HOL_R,   HOL_R,   _______, DUAL_QU, HOL_QTE, _______, HOL_TRM
 ),
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_RBRC, KC_LBRC, KC_BSLS, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
