ed8c31e467cab666ca4ada23dd38d2db040512dd
Alejandro Gomez-Londono <alegomez544@gmail.com>
2019-07-24 16:44:40 +0200

    [agomezl/planck] Fix issue with layout + added extra CTRL in MOVE layer

--- keyboards/planck/keymaps/agomezl/keymap.c
+++ keyboards/planck/keymaps/agomezl/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* BASE
  */
-[BASE] = layout_planck_grid(
+[BASE] = LAYOUT_planck_grid(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         C_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, C_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         L_R_BRC, KC_VOLD, KC_VOLU, KC_END,  _______, _______, HOL_Y,   KC_PGUP, KC_UP,   KC_PGDN, HOL_P,   HOL_THN,
         _______, KC_HOME, KC_SPC,  KC_DEL,  HOL_F  , _______, HOL_H,   KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
         _______, _______, HOL_X,   E_LAST,  MO_MOUS, _______, _______, _______, _______, _______, _______, L_R_PAR,
-        _______, _______, E_MAGIT, _______, _______, HOL_R,   HOL_R,   _______, _______, _______, _______, HOL_TRM
+        _______, _______, E_MAGIT, _______, _______, HOL_R,   HOL_R,   KC_LCTL, _______, _______, _______, HOL_TRM
 ),
 
 /* MUM
