7ab0039dd7b326160785b3727254c13fc870f7c7
Alejandro Gomez-Londono <alegomez544@gmail.com>
2019-07-24 16:56:27 +0200

    [agomezl/planck] Move brackets and some macros around

--- keyboards/planck/keymaps/agomezl/keymap.c
+++ keyboards/planck/keymaps/agomezl/keymap.c
@@ -95,16 +95,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         C_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, C_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LOCK, _______, KC_LGUI, KC_LALT, MO_MOVE, KC_SPC,  KC_SPC,  OSL_NUM, KC_QUOT, KC_GRV,  MO(SYM), RUN_M1
+        KC_LOCK, _______, KC_LGUI, KC_LALT, MO_MOVE, KC_SPC,  KC_SPC,  OSL_NUM, KC_LPRN, KC_RPRN, MO(SYM), RUN_M1
 ),
 
 /* MOVEMENT
  */
 [MOVE] = LAYOUT_planck_grid(
-        L_R_BRC, KC_VOLD, KC_VOLU, KC_END,  _______, _______, HOL_Y,   KC_PGUP, KC_UP,   KC_PGDN, HOL_P,   HOL_THN,
-        _______, KC_HOME, KC_SPC,  KC_DEL,  HOL_F  , _______, HOL_H,   KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
+        KC_GRV,  KC_VOLD, KC_VOLU, KC_END,  _______, HOL_TRM, HOL_Y,   KC_PGUP, KC_UP,   KC_PGDN, HOL_P,   HOL_THN,
+        KC_QUOT, KC_HOME, KC_SPC,  KC_DEL,  HOL_F  , _______, HOL_H,   KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
         _______, _______, HOL_X,   E_LAST,  MO_MOUS, _______, _______, _______, _______, _______, _______, L_R_PAR,
-        _______, _______, E_MAGIT, _______, _______, HOL_R,   HOL_R,   KC_LCTL, _______, _______, _______, HOL_TRM
+        _______, _______, E_MAGIT, _______, _______, HOL_R,   HOL_R,   KC_LCTL, DUAL_QU, HOL_QTE, _______, L_R_BRC
 ),
 
 /* MUM
@@ -113,8 +113,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-        _______, DUAL_QU, HOL_QTE, ML_CMNT, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        _______, DUAL_QU, HOL_QTE, ML_CMNT, _______, _______, _______, _______, _______, _______, KC_BSLS, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______
 ),
 
 [MOUS] = LAYOUT_planck_grid(
