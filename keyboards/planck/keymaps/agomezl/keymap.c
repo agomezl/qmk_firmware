4d8e16a8fee498a0b5e513ab3f52f8f2ab4221bb
Alejandro Gòmez-Londoño <agomezl@pm.me>
2020-09-23 22:24:53 +0200

    Update planck

--- keyboards/planck/keymaps/agomezl/keymap.c
+++ keyboards/planck/keymaps/agomezl/keymap.c
@@ -121,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         L_R_BRC, KC_VOLD, KC_VOLU, KC_END,  HOL_APP, HOL_THN, HOL_Y,   KC_PGUP, KC_UP,   KC_PGDN, HOL_P,   DUAL_Q2,
         KC_QUOT, KC_HOME, KC_SPC,  KC_DEL,  HOL_F  , KC_ESC,  HOL_H,   KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  _______,
         OSM_S,   ML_CMNT, HOL_X,   E_LAST,  MO(LY4), _______, _______, _______, _______, _______, _______, L_R_PAR,
-        C_S_OSM, _______, _______, _______, _______, HOL_R,   HOL_R,   OSM_C,   DUAL_QU, HOL_QTE, _______, REC_M1
+        C_S_OSM, _______, _______, _______, _______, HOL_R,   HOL_R,   OSM_C,   DUAL_QU, HOL_QTE, STOP_M,  REC_M1
 ),
 
 /* MUM
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         OSM_S,   _______, _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_RCBR, _______, KC_BSLS, _______,
-        M_S_OSM, _______, _______, _______, OSM_C,   _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______
+        M_S_OSM, _______, _______, _______, OSM_C,   _______, _______, _______, KC_LBRC, KC_RBRC, _______, RESET
 ),
 
 [LY4] = LAYOUT_planck_grid(
