8232cb2d38ceeb5d702c84d0ee558586c17558fa
Alejandro Gòmez-Londoño <agomezl@pm.me>
2020-10-13 13:51:38 +0200

    Move arrows around

--- keyboards/moonlander/keymaps/agomezl/keymap.c
+++ keyboards/moonlander/keymaps/agomezl/keymap.c
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    HOL_TRM, /* */ DUAL_QU, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         C_TAB,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    L_R_BRC, /* */ WEB_TAG, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, C_ENT,
         KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             /* */          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-        KC_GRV,  RGB_MOD, E_MAGIT, RGB_TOG, OSM_G,            RUN_M1,  /* */ RUN_M2,           OSL_LY2, KC_UP,   KC_DOWN, KC_LEFT, KC_RIGHT,
+        KC_GRV,  RGB_MOD, E_MAGIT, RGB_TOG, OSM_G,            RUN_M1,  /* */ RUN_M2,           OSL_LY2, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT,
                                             OSM_M, OSL_LY1,   KC_LBRC, /* */ KC_RBRC, KC_BSPC, KC_SPC
     ),
 
